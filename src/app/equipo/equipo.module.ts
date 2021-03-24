ddb2ed81dc36c93d8e4dea33783c4f72f7289b97
Ildefonso <ponchovalo@outlook.com>
2021-03-24 15:22:13 -0600

    equipo_component

--- src/app/equipo/equipo.module.ts
+++ src/app/equipo/equipo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EquiposComponent } from './equipos/equipos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EquiposComponent],
   imports: [
     CommonModule
   ]
