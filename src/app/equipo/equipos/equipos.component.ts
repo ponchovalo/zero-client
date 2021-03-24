600e35630e41bd816ce96484d64950ee9708dfe2
Ildefonso <ponchovalo@outlook.com>
2021-03-24 16:26:00 -0600

    equipo_table

--- src/app/equipo/equipos/equipos.component.ts
+++ src/app/equipo/equipos/equipos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Equipo } from '../interfaces/equipo';
 
 @Component({
   selector: 'app-equipos',
@@ -8,6 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class EquiposComponent implements OnInit {
 
   constructor() { }
+
+  equipo: Equipo;
+
+  equipos: Equipo[] = [
+    {
+      idequipo: 1,
+      ubicacionBlade: '1-1-1',
+      serieBlade: 'LDT004257',
+      ipBlade: '10.222.22.10',
+      macBlade: '0005.8250.8afc',
+      ipSwBlade: 'string',
+      puertoBlade: 'string'
+    },
+    {
+      idequipo: 2,
+      ubicacionBlade: '1-1-2',
+      serieBlade: 'LDT004257',
+      ipBlade: '10.222.22.10',
+      macBlade: '0005.8250.8afc',
+      ipSwBlade: 'string',
+      puertoBlade: 'string'
+    },
+
+  ];
 
   ngOnInit(): void {
   }
---
+++ src/app/equipo/interfaces/equipo.ts
@@ -0,0 +1,9 @@
+export interface Equipo {
+    idequipo: number;
+    ubicacionBlade: string;
+    serieBlade: string;
+    ipBlade: string;
+    macBlade: string;
+    ipSwBlade: string;
+    puertoBlade: string;
+}
